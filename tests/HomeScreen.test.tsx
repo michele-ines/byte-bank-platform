a6c14febaecb89d2f0f78ad9bda35557e3dbbbbf
michele-ines <minesck@hotmail.com>
2025-08-12 10:44:26 -0300

    chore: ignore Jest coverage; keep only test changes

--- tests/HomeScreen.test.tsx
+++ tests/HomeScreen.test.tsx
@@ -1,11 +1,26 @@
+// tests/HomeScreen.test.tsx
+import HomeScreen from "@/src/features/home/HomeScreen";
 import { render } from "@testing-library/react-native";
 
-import HomeScreen from "@/app/(tabs)/index";
-
 describe("<HomeScreen />", () => {
-  test("Text renders correctly on HomeScreen", () => {
-    const { getByText } = render(<HomeScreen />);
+  it("renderiza o herói e o formulário de login", () => {
+    const { getByText, getByPlaceholderText, getByRole } = render(<HomeScreen />);
 
-    getByText("Welcome!");
+    // Hero / mensagens
+    getByText(/experimente mais liberdade/i);
+    getByText(/crie sua conta/i);
+
+    // Form
+    getByText(/^login$/i);
+    getByText(/^e-mail$/i);
+    getByPlaceholderText("Digite seu email cadastrado");
+    getByText(/^senha$/i); // <-- ancorado para não pegar o link
+    getByPlaceholderText("Digite sua senha");
+
+    // Link "Esqueci a senha!"
+    getByRole("link", { name: /^esqueci a senha!?$/i });
+
+    // Botão
+    getByText(/^acessar$/i);
   });
 });
