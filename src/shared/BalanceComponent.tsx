3868ebe89af8395e080fafc733ac90b3bdf350a7
Larissa Storck <larissastorck@hotmail.com>
2025-08-25 22:31:28 -0300

    wip

---
+++ src/shared/BalanceComponent.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native"
+
+export default function Balance() {
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
